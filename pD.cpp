b13e14ff815d07f27f2478e2e6d2008308b49a34
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-22 23:04:36 +0600

    Create pD.cpp

---
+++ pD.cpp
@@ -0,0 +1,30 @@
+/*
+* Course: Competitive Programming Sessional (CCE-2310)
+* Assignment: Codeforces ICPC Assiut University Community
+* Problem ID: D
+* Author: Sultana Jahan Tahmina (ID:E243419)
+* Language/Version: GNU G++20 13.2 (64 bit, winlibs)
+* Codeforces Profile: https://codeforces.com/profile/hscsultana22
+* Date: 2025-11-22
+* Statement: 
+https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x;
+    while (cin >> x) {
+        if (x == 1999) {
+            cout << "Correct\n";
+            return 0;
+        } else {
+            cout << "Wrong\n";
+        }
+    }
+    return 0;
+}
