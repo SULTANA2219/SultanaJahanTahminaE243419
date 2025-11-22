f4796db3278ff6c9e4be4dcf680b8d95de24b28b
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-22 23:19:24 +0600

    Create p3.cpp

---
+++ p3.cpp
@@ -0,0 +1,25 @@
+/*
+* Course: Competitive Programming Sessional (CCE-2310)
+* Assignment: Codeforces ICPC Assiut University Community
+* Problem ID: 3
+* Author: Sultana Jahan Tahmina (ID:E243419)
+* Language/Version: GNU G++20 13.2 (64 bit, winlibs)
+* Codeforces Profile: https://codeforces.com/profile/hscsultana22
+* Date: 2025-11-22
+* Statement: 
+ALL COMMON PATTERN TYPES.pdf/3
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++){
+        cout << string(N - i, ' ');
+        cout << string(2 * i - 1, '*');
+        cout << "\n";
+    }
+}
